bd8dbf69eacc7f33c754a8560f3b2ba0e451ede8
feodro <feodorryabov958@gmail.com>
2022-02-02 17:29:15 +0300

    функции для получения изображения

--- xz.py
+++ xz.py
@@ -9,15 +9,14 @@ def degree(obj):
            str(abs((float(left_bottom[1]) - float(right_upper[1])) / 2))
 
 
-def show_image():
+def show_image(x_coor, y_coor):
     geocoder_params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": " ".join(sys.argv[1:]),
+        "geocode": " ",
         "format": "json"}
     response = requests.get("http://geocode-maps.yandex.ru/1.x/", params=geocoder_params)
     toponym = response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_longitude, toponym_lattitude = toponym["Point"]["pos"].split(" ")
-    print(toponym_longitude, toponym_lattitude)  # корды
+    toponym_longitude, toponym_lattitude = x_coor, y_coor
     spn = degree(toponym)
     map_params = {
         "ll": ",".join([toponym_longitude, toponym_lattitude]),  # корды
